33ed93a7c2370ed4a10d82c8932b8321cd95493d
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-11-19 19:11:59 -0300

    feat(observer pattern): implemented observer in the project

--- MonsterBattleGame/src/main/java/br/squad3/observer/ConsoleObserver.java
+++ MonsterBattleGame/src/main/java/br/squad3/observer/ConsoleObserver.java
@@ -1,5 +1,7 @@
 package br.squad3.observer;
 
+import br.squad3.observer.interfaces.GameObserver;
+
 public class ConsoleObserver implements GameObserver {
     @Override
     public void update(String message) {
--- MonsterBattleGame/src/main/java/br/squad3/observer/interfaces/GameObserver.java
+++ MonsterBattleGame/src/main/java/br/squad3/observer/interfaces/GameObserver.java
@@ -1,4 +1,4 @@
-package br.squad3.observer;
+package br.squad3.observer.interfaces;
 
 public interface GameObserver {
     void update(String message);
